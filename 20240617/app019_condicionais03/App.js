0175048a7a3f741165d33d260332fd28152097c1
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-06-25 11:21:34 -0300

    Desafio 3 das condicionais pronto.

--- 20240617/app019_condicionais03/App.js
+++ 20240617/app019_condicionais03/App.js
@@ -6,6 +6,7 @@ import {View, Text, Image} from 'react-native';
 function App() {
   const corPrimaria = "#43b941";
   const corSecundaria = "lightgray";
+  const secundariaAlt = "rgb(128, 128, 128)";
   const corTercearia = "darkblue";
   const corQuaternaria = "#e6e6e6";
 
@@ -27,8 +28,11 @@ function App() {
   iconeVoltar = require('./img/arrow-left-solid.svg');
   iconePesquisar = require('./img/pesquisar-claro.svg');
   iconePesquisar2 = require('./img/pesquisar-escuro.svg');
+  iconeMenu = require('./img/bars-solid.svg');
+  iconeLink = require('./img/ir-site.svg');
 
   let textoPesquisa = "Busque em Locais";
+  let bairro = "Portão";
 
   let alimentacao, alvaras, assistencia, capacitacao, cidadania, concursos, cultura, direitos, educacao, emergencias, esporte, habitacao;
   alimentacao = require('./img/alimentacao.png');
@@ -43,6 +47,20 @@ function App() {
   emergencias = require('./img/emergencias.png');
   esporte = require('./img/esportes.png');
   habitacao = require('./img/habitacao.png');
+
+  let logoCuritiba = require('./img/curitiba.png');
+
+  let localCategoria, linkMaps, local1, local2, local3, diaSemana, temperatura, localCategoria2, local4;
+
+  localCategoria = "Academia ao Ar Livre";
+  localCategoria2 = "Departamento";
+  linkMaps = "Abrir no Google Mapas";
+  local1 = "Academia ao Ar Livre Artur Bernardes 2";
+  local2 = "Academia ao Ar Livre Mundiais";
+  local3 = "Academia ao Ar Livre 29 de Março";
+  local4 = "Abc Trânsito"
+  diaSemana = "Ter.";
+  temperatura = "12°"
 
   if ( pagina == "Serviços" ) {
     return (
@@ -209,23 +227,75 @@ function App() {
 
         <View style={{ flex: 1, backgroundColor: corQuaternaria, paddingHorizontal: 20 }}>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 20 }}>
-            <Image source={ require('./img/bars-solid.svg') } style={{ width: 25, height: 25 }}></Image>
+            <Image source={ iconeMenu } style={{ width: 25, height: 25 }}></Image>
 
-            <Image source={ require('./img/curitiba.png') }></Image>
+            <Image source={ logoCuritiba }></Image>
 
             <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-              <Text>Seg.</Text>
-              <Text style={{ fontWeight: 'bold', marginLeft: 10 }}>22°</Text>
+              <Text style={{ color: secundariaAlt }}>{diaSemana}</Text>
+              <Text style={{ fontWeight: 'bold', marginLeft: 10 }}>{temperatura}</Text>
             </View>   
           </View>
 
           <View style={{ alignItems: 'center', padding: '1em' }}>
-            <Text style={{ fontSize: '1.3em', fontWeight: 'bold', color: 'green' }}>{pagina2}</Text>
+            <Text style={{ fontSize: '1.3em', fontWeight: 'bold', color: corPrimaria }}>{pagina2}</Text>
           </View>
 
           <View style={{ flexDirection: 'row', backgroundColor: corSecundaria, padding: '1em', justifyContent: 'space-between' }}>
-            <Text>{textoPesquisa}</Text>
+            <Text style={{ color: secundariaAlt }}>{textoPesquisa}</Text>
             <Image source={ iconePesquisar2 } style={{ width: 20, height: 20 }}></Image>
+          </View>
+          
+          <View style={{ flexDirection: 'row', backgroundColor: corQuaternaria, padding: '1em', justifyContent: 'flex-end', borderColor: corSecundaria, borderWidth: 2, marginVertical: 15 }}>
+            <Text style={{ fontWeight: 'bold', paddingRight: 10 }}>{bairro}</Text>
+          </View>
+
+          <View style={{ borderColor: corSecundaria, borderTopWidth: 2 }}>
+            <View>
+              <Text style={{ fontWeight: 'bold', color: secundariaAlt, paddingVertical: '0.7em' }}>{localCategoria}</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: '1.4em', color: corPrimaria }}>{local1}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', paddingVertical: '0.7em' }}>
+              <Text style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{linkMaps}</Text>
+              <Image source={ iconeLink } style={{ width: 20, height: 20, marginLeft: 10 }}></Image>
+            </View>
+          </View>
+
+          <View style={{ borderColor: corSecundaria, borderTopWidth: 2 }}>
+            <View>
+              <Text style={{ fontWeight: 'bold', color: secundariaAlt, paddingVertical: '0.7em' }}>{localCategoria}</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: '1.4em', color: corPrimaria }}>{local2}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', paddingVertical: '0.7em' }}>
+              <Text style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{linkMaps}</Text>
+              <Image source={ iconeLink } style={{ width: 20, height: 20, marginLeft: 10 }}></Image>
+            </View>
+          </View>
+
+          <View style={{ borderColor: corSecundaria, borderTopWidth: 2 }}>
+            <View>
+              <Text style={{ fontWeight: 'bold', color: secundariaAlt, paddingVertical: '0.7em' }}>{localCategoria}</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: '1.4em', color: corPrimaria }}>{local3}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', paddingVertical: '0.7em' }}>
+              <Text style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{linkMaps}</Text>
+              <Image source={ iconeLink } style={{ width: 20, height: 20, marginLeft: 10 }}></Image>
+            </View>
+          </View>
+
+          <View style={{ borderColor: corSecundaria, borderTopWidth: 2 }}>
+            <View>
+              <Text style={{ fontWeight: 'bold', color: secundariaAlt, paddingVertical: '0.7em' }}>{localCategoria2}</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: '1.4em', color: corPrimaria }}>{local4}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', paddingVertical: '0.7em' }}>
+              <Text style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{linkMaps}</Text>
+              <Image source={ iconeLink } style={{ width: 20, height: 20, marginLeft: 10 }}></Image>
+            </View>
           </View>
         </View>
 
@@ -233,19 +303,19 @@ function App() {
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image source={ iconeB1 } style={{ width: 25, height: 30 }}></Image>
 
-            <Text style={{ color: corSecundaria }}>Notificações</Text>
+            <Text style={{ color: corSecundaria }}>{botao1}</Text>
           </View>
 
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image source={ iconeB2 } style={{ width: 35, height: 30 }}></Image>
 
-            <Text style={{ color: corPrimaria }}>Início</Text>
+            <Text style={{ color: corPrimaria }}>{botao2}</Text>
           </View>
 
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image source={ iconeB3 } style={{ width: 30, height: 30 }}></Image>
 
-            <Text style={{ color: corSecundaria }}>Configurações</Text>
+            <Text style={{ color: corSecundaria }}>{botao3}</Text>
           </View>
         </View>
       </View>
