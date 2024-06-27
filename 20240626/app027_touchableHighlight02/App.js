46630966080dd39bb90fa323c92cd4136a377228
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-06-27 10:03:07 -0300

    Desafio 2 do touchable highlight pronto.

--- 20240626/app027_touchableHighlight02/App.js
+++ 20240626/app027_touchableHighlight02/App.js
@@ -9,24 +9,48 @@ function proximo() {
 }
 
 function anterior() {
-  alert("Você clicou no botão anterior");
+  alert("Você clicou no botão pular");
 }
 
 function App() {
   let desenho = require('./assets/celular.webp');
   let titulo = "Bem vindo(a) ao Aplicativo Barilife!";
   let introducao = "A Sociedade Brasileira de Cirurgia Bariatrica e Metabolica (SBCBM) desenvolveu o aplicativo pensando em você, paciente bariatrico. É um aplicativo inovador e gratuito.";
+  let botaoEsq = 'Pular';
+  let botaoDir = "Próximo";
 
   return (
     <View style={ estilos.fundo }>
-      <View style={ estilos.viewImagem }>
-        <Image source={ desenho } style={ estilos.desenho }></Image>
+      <View style={{ flex: 1 }}>
+        <View style={ estilos.viewImagem }>
+          <Image source={ desenho } style={ estilos.desenho }></Image>
+        </View>
+
+        <View style={ estilos.viewIntro }>
+          <Text style={ estilos.titulo }>{titulo}</Text>
+
+          <Text style={ estilos.textoIntro }>{introducao}</Text>
+        </View>
+
+        <View style={ estilos.viewBotoes }>
+          <TouchableHighlight onPress={ anterior } style={ estilos.botoes }>
+
+            <Text style={[ estilos.textoBotoes, { color: '#2560be' } ]}>{botaoEsq}</Text>
+            {/* Usando um array para usar mais de um estilo para o componente, inclusive juntando o inline com o estilo externo. */}
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={ proximo } style={[ estilos.botoes, estilos.botaoDir ]}>
+          {/* Utilizando dois estilos externos. */}
+
+            <Text style={[ estilos.textoBotoes, { textAlign: 'center', color: 'white' } ]}>{botaoDir}</Text>
+          </TouchableHighlight>
+        </View>
       </View>
 
-      <View style={ estilos.viewIntro }>
-        <Text style={ estilos.titulo }>{titulo}</Text>
-
-        <Text>{introducao}</Text>
+      <View style={ estilos.viewPontos }>
+        <View style={ estilos.pontoAzul }></View>
+        <View style={ estilos.pontoCinza }></View>
+        <View style={ estilos.pontoCinza }></View>
       </View>
     </View>
   )
--- 20240626/app027_touchableHighlight02/estilos.js
+++ 20240626/app027_touchableHighlight02/estilos.js
@@ -26,8 +26,64 @@ const estilos = StyleSheet.create({
     
     titulo: {
         fontWeight: 'bold',
-        fontSize: '2em'
-    }
+        fontSize: '2em',
+        marginBottom: '0.5em',
+        textAlign: 'justify'
+    },
+
+    textoIntro: {
+        textAlign: 'justify',
+        color: 'gray',
+    },
+
+    viewPontos: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItens: 'center'
+    },
+
+    pontoCinza: {
+        width: 16,
+        height: 16,
+        borderRadius: 50,
+        backgroundColor: 'lightgray',
+        margin: '0.4em'
+    },
+
+    pontoAzul: {
+        width: 16,
+        height: 16,
+        borderRadius: 50,
+        backgroundColor: '#2560be',
+        margin: '0.4em'
+    },
+
+    viewBotoes: {
+        flexDirection: 'row',
+        justifyContent: 'center', 
+        alignItems: 'center',
+        marginTop: 20
+    },
+
+    botoes: {
+        borderWidth: 1, 
+        borderRadius: 50, 
+        borderColor: '#2560be', 
+        fontSize: '1.2em', 
+        width: '35%', 
+        textAlign: 'center', 
+        padding: '1em',
+        marginHorizontal: '0.5em'
+    },
+
+    botaoDir: {
+        backgroundColor: '#2560be'
+    },
+
+    textoBotoes: {
+        textAlign: 'center', 
+        fontWeight: 'bold'
+    },
 })
 
 export { estilos };
