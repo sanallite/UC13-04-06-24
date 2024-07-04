43fe4f47d84e1d1fd7420816fd4d7016f60e6da0
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-07-04 11:55:24 -0300

    Meu desafio do states pronto, só falta comentar

---
+++ 20240702/app031_states_Hololive/estilos.js
@@ -0,0 +1,78 @@
+/* Aula 12 - Meu desafio usando states - Folha de estilos */
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: 'blueviolet'
+    },
+
+    viewTitle: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        margin: '1rem',
+        padding: '1rem'
+    },
+
+    title: {
+        /* textAlign: 'center', */
+        fontWeight: 'bold',
+        fontSize: '1.1rem',
+    },
+
+    subtitulo: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: '1.2rem'
+    },
+
+    viewTalento: {
+        flex: 0.38,
+        alignItems: 'center',
+        padding: '0 1.25rem 1.25rem 1.25rem',
+    },
+
+    textosDestaque: {
+        color: 'white',
+        fontSize: '1.25rem',
+        marginBottom: '1.25rem'
+    },
+
+    imagemTalento: {
+        marginVertical: '1.25rem',
+        width: 300,
+        height: 300,
+        resizeMode: 'contain',
+        borderRadius: 10
+    },
+
+    viewBotao: {
+        alignItems: 'center',
+        marginBottom: '1.25rem'
+    },
+
+    botaoJogar: {
+        backgroundColor: 'lightblue',
+        borderRadius: 10,
+        padding: '0.5rem',
+        width: '50%',
+        alignItems: 'center'
+    },
+
+    viewDetalhes: {
+        flex: 0.62,
+        padding: '1.25rem',
+        margin: '1.25rem',
+        backgroundColor: 'orange',
+        borderRadius: 10
+    },
+
+    lista: {
+        color: 'white',
+        marginBottom: '0.5rem',
+        fontSize: '1.1rem'
+    }
+});
+
+export { styles };
